28a4126a572c452f396f091e8b677a2f21420164
tstoddart <1420767+tstoddart@users.noreply.github.com>
2024-12-02 17:02:30 +0000

    remove numpy

--- day1/1.py
+++ day1/1.py
@@ -1,4 +1,4 @@
-import numpy as np
+#import numpy as np
 with open('input.txt') as f:
         lines = f.read().splitlines()
 
@@ -6,7 +6,8 @@ full_list = [ int(x) for xs in [i.split() for i in lines] for x in xs  ]
 list1 = sorted(full_list[1::2])
 list2 = sorted(full_list[0::2])
 
-part1 = sum([abs(x) for x in np.subtract(list1,list2)])
+part1 = sum([abs(x) for x in map(int.__sub__, list1, list2)])
 part2 = sum([x*list2.count(x) for x in list1])
 
-part1, part2
+print("part 1 =", part1)
+print("part 2 =", part2)
